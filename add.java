86fedfe42d3dc7867f83eca71283df738728d31d
newor01 <86951187+newor01@users.noreply.github.com>
2021-07-07 14:10:27 +0800

    Add files via upload

---
+++ add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class add
+{ public static void main (String[] args){
+    int a,b,c;
+    Scanner inputNumScanner = new Scanner(System.in);
+    System.out.println("Enter two numbers :");
+    a=inputNumScanner.nextInt();
+    b=inputNumScanner.nextInt();
+    c=a+b;
+    System.out.println(a+"+"+b+"="+c);
+}
+}
+
+
+
+    
+    
+    
